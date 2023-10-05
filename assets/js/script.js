34db08c06e8a6237f7d9e5cb0d0347553cdb9fe2
Tomas <tomassilvera99@gmail.com>
2023-10-05 12:58:33 -0300

    Seccion Propiedades

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,3 +14,13 @@ const /** {NodeElement} */ $header = document.querySelector("[data-header]");
 window.addEventListener("scroll", e => {
   $header.classList[window.scrollY > 50 ? "add" : "remove"]("active");
 });
+
+/** Boton de favoritos */
+
+const /** {Nodelist} */ $toggleBtns = document.querySelectorAll("[data-toggle-btn]");
+
+$toggleBtns.forEach($toggleBtns => {
+  $toggleBtns.addEventListener("click", () => {
+    $toggleBtns.classList.toggle("active");
+  });  
+});
